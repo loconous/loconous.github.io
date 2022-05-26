a36fbe45188a66e323e3888c49e235362dbd5624
loconous <72740721+loconous@users.noreply.github.com>
2022-05-26 13:02:43 -0400

    finished sections, hidding scroll, topheader, adding color pallette

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,14 @@
+import "./topbar.scss"
+
+export default function Topbar() {
+  return (
+    <div className="topbar" id="topbar">
+         <div className="wrapper">
+           <div className="left">
+             <a href=""></a>
+           </div>
+           <div className="right"></div>
+         </div>
+    </div>
+  )
+}
