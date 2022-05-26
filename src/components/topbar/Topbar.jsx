2ea7646c18d19bf9509625d6ca4d029c091ab0d5
loconous <72740721+loconous@users.noreply.github.com>
2022-05-26 14:05:50 -0400

    added header info

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,13 +1,26 @@
 import "./topbar.scss"
+import PhoneAndroidIcon from '@mui/icons-material/PhoneAndroid';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
 
 export default function Topbar() {
   return (
     <div className="topbar" id="topbar">
          <div className="wrapper">
            <div className="left">
-             <a href=""></a>
+             <a href="#intro" className="logo">
+               loconous.
+             </a>
+             <div className="itemContainer">
+               <PhoneAndroidIcon className="icon"/>
+                <span>+1 (865) 963-8208</span>
+             </div>
+             <div className="itemContainer">
+               <MailOutlineIcon className="icon"/>
+                <span>lgonzalvill@gmail.com</span>
+             </div>
            </div>
-           <div className="right"></div>
+           <div className="right">
+           </div>
          </div>
     </div>
   )
