aaf39170ae01cd072c75d015bbab98fc3aaf854d
loconous <72740721+loconous@users.noreply.github.com>
2022-05-26 15:44:54 -0400

    added menu buton and transition for topbar

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import Work from "./components/work/Work";
 import Portfolio from "./components/portfolio/Portfolio";
 import Testimonials from "./components/testimonials/Testimonials";
 import Contact from "./components/contact/Contact";
-import "./app.scss"
+import "./app.scss";
+import { useState} from "react";
 
 function App() {
+  const [menuOpen,setMenuOpen] = useState(false)
   return (
     <div className="app">
-      <Topbar/>
+      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
       <div className="sections">
         <Intro/>
         <Work/>
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -2,24 +2,29 @@ import "./topbar.scss"
 import PhoneAndroidIcon from '@mui/icons-material/PhoneAndroid';
 import MailOutlineIcon from '@mui/icons-material/MailOutline';
 
-export default function Topbar() {
+export default function Topbar({ menuOpen, setMenuOpen }) {
   return (
-    <div className="topbar" id="topbar">
+    <div className={"topbar " + (menuOpen && "active ")}>
          <div className="wrapper">
            <div className="left">
-             <a href="#intro" className="logo">
-               loconous.
-             </a>
-             <div className="itemContainer">
-               <PhoneAndroidIcon className="icon"/>
-                <span>+1 (865) 963-8208</span>
-             </div>
-             <div className="itemContainer">
-               <MailOutlineIcon className="icon"/>
-                <span>lgonzalvill@gmail.com</span>
-             </div>
+              <a href="#intro" className="logo">
+                loconous.
+              </a>
+              <div className="itemContainer">
+                <PhoneAndroidIcon className="icon"/>
+                  <span>+1 (865) 963-8208</span>
+              </div>
+              <div className="itemContainer">
+                <MailOutlineIcon className="icon"/>
+                  <span>lgonzalvill@gmail.com</span>
+              </div>
            </div>
            <div className="right">
+              <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
+                  <span className="line1"></span>
+                  <span className="line2"></span>
+                  <span className="line3"></span>
+              </div>
            </div>
          </div>
     </div>
