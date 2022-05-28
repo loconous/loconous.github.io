63f3e820de16b40fba85839bc19071ac1c65e44a
loconous <72740721+loconous@users.noreply.github.com>
2022-05-28 14:48:18 -0400

    finished intro, need to change pic only

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,8 +1,30 @@
-import "./intro.scss"
-import { init } from 'ityped'
+import "./intro.scss";
+import { init } from 'ityped';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import { useEffect, useRef } from 'react';
 
 export default function Intro() {
+
+  const textRef = useRef();
+
+  useEffect(() => {
+    //console.log(textRef);
+    const options = {
+    	strings: [
+        'Security Professional',
+        'Software Developer',
+        'Embedded Systems Developer',
+        'Researcher'
+      ],
+      showCursor: true,
+      //typeSpeed: 50,
+      backSpeed: 60,
+    };
+
+    init(textRef.current, options);
+
+  }, []);
+
   return (
     <div className="intro" id="intro">
         <div className="left">
@@ -14,9 +36,10 @@ export default function Intro() {
           <div className="wrapper">
             <h2>Hola! Hello! I'm </h2>
             <h1>Luis A. González Villalobos</h1>
-            <h3>Computer Engineer <span></span></h3>
+            <h3>Ingeniero en Computación / Computer Engineer</h3>
+            <span ref={textRef}></span>
           </div>
-          <a href="#portfolio">
+          <a href="#work">
             <div className="itemContainer">
               <ArrowDropDownIcon className="icon"/>
             </div>
